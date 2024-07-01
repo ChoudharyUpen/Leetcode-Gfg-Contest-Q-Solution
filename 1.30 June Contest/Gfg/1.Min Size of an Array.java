3134d112c4e51d2cd76a7ccf61a292af50b43c3d
ChoudharyUpen <141799025+ChoudharyUpen@users.noreply.github.com>
2024-07-01 09:46:29 +0530

    Create 1.Min Size of an Array.java

---
+++ 1.30 June Contest/Gfg/1.Min Size of an Array.java
@@ -0,0 +1,21 @@
+// if there is a dominating element, then it will eat up rest of the el
+// if there is no dominating el, then by observation
+// every el will probably eat up other el
+// thus n = even, ans is 0 else 1
+
+class Solution {
+    public static int MinSize(int n, int[] a) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int max = 0;
+        for(int i=0; i<n; i++)
+        {
+             map.put(a[i], map.getOrDefault(a[i], 0)+1);
+             max = Math.max(max, map.get(a[i]));
+        }
+        
+        if(max > n/2)
+        return max-(n-max);
+        
+        return (n&1)==0 ?  0 : 1;
+    }
+}
