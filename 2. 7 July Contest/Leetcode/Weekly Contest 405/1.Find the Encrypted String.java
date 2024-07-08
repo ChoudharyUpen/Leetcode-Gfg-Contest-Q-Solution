431f7cdcb58f4a33a4ab38d7615b4365b6274961
ChoudharyUpen <141799025+ChoudharyUpen@users.noreply.github.com>
2024-07-08 05:42:46 +0530

    Create 1.Find the Encrypted String.java

---
+++ 2. 7 July Contest/Leetcode/Weekly Contest 405/1.Find the Encrypted String.java
@@ -0,0 +1,9 @@
+class Solution {
+  //s = "dart", k = 3
+    public String getEncryptedString(String s, int k) {
+        size = s.length();  // size = 4
+        k %= size;  // k = 3 % 4 = 3
+        s = s + s;  // s = "dart" + "dart" = "dartdart"
+        return s.substring(k, k + size);  // s.substring(3, 3 + 4) = s.substring(3, 7) = "tdar"
+    }
+}
